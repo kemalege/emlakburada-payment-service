328c505f8ca6daadecbc3d8f4bb054ea23599d46
kemalege <cirpanliege@gmail.com>
2024-07-13 00:19:12 +0300

    Create emlakburada payment service

---
+++ src/main/java/com/patika/emlakburadapaymentservice/EmlakburadaPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patika.emlakburadapaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlakburadaPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmlakburadaPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patika/emlakburadapaymentservice/EmlakburadaPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.emlakburadapaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmlakburadaPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
