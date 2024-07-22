f7c79e5262402b2276041a38251f31605a5ffb75
kemalege <cirpanliege@gmail.com>
2024-07-22 04:43:31 +0300

    Create controller getUserPaymentsById

--- src/main/java/com/patika/emlakburadapaymentservice/controller/PaymentController.java
+++ src/main/java/com/patika/emlakburadapaymentservice/controller/PaymentController.java
@@ -3,9 +3,12 @@ package com.patika.emlakburadapaymentservice.controller;
 import com.patika.emlakburadapaymentservice.dto.request.PackagePaymentRequest;
 import com.patika.emlakburadapaymentservice.dto.response.GenericResponse;
 import com.patika.emlakburadapaymentservice.dto.response.PaymentResponse;
+import com.patika.emlakburadapaymentservice.model.Payment;
 import com.patika.emlakburadapaymentservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +21,11 @@ public class PaymentController {
     public GenericResponse<PaymentResponse> pay(@RequestBody PackagePaymentRequest request){
         PaymentResponse paymentResponse = paymentService.pay(request);
         return GenericResponse.success(paymentResponse);
+    }
+
+    @GetMapping("list/user/{id}")
+    public GenericResponse<List<Payment>> getUserPaymentsById(@PathVariable Long id) {
+        return GenericResponse.success(paymentService.getPaymentsByUserId(id));
     }
 
 }
